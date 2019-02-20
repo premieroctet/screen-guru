c3629962558792340f1677d0235b23858fb4c403
Baptiste Adrien <adrien.baptiste@gmail.com>
2019-02-20 16:08:15 +0100

    Upgrade React (#11)

--- src/components/SettingsPanel/SettingsPanel.js
+++ src/components/SettingsPanel/SettingsPanel.js
@@ -1,52 +1,36 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { TwitterPicker } from 'react-color';
 import { ColorLink, Popover, Overlay, Panel } from './elements';
 
-class SettingsPanel extends PureComponent {
-  state = {
-    displayColorPicker: false,
-  };
+const SettingsPanel = props => {
+  const [showColorPicker, setShowColorPicker] = useState(false);
 
-  handleClick = () => {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker });
-  };
+  return (
+    <Panel>
+      <ColorLink
+        disabled={props.noBackground}
+        onClick={() => setShowColorPicker(!showColorPicker)}
+        className="color-link"
+        borderColor={props.color}
+      >
+        Pick the background color
+      </ColorLink>
 
-  handleClose = () => {
-    this.setState({ displayColorPicker: false });
-  };
+      <label>
+        <input value={props.noBackground} onChange={e => props.onToggleBackground(e.target.value)} type="checkbox" /> No
+        background
+      </label>
 
-  render() {
-    return (
-      <Panel>
-        <ColorLink
-          disabled={this.props.noBackground}
-          onClick={this.handleClick}
-          className="color-link"
-          borderColor={this.props.color}
-        >
-          Pick the background color
-        </ColorLink>
-
-        <label>
-          <input
-            value={this.props.noBackground}
-            onChange={e => this.props.onToggleBackground(e.target.value)}
-            type="checkbox"
-          />{' '}
-          No background
-        </label>
-
-        {this.state.displayColorPicker ? (
-          <Popover>
-            <Overlay onClick={this.handleClose} />
-            <TwitterPicker {...this.props} />
-          </Popover>
-        ) : null}
-      </Panel>
-    );
-  }
-}
+      {showColorPicker && (
+        <Popover>
+          <Overlay onClick={() => setShowColorPicker(false)} />
+          <TwitterPicker {...props} />
+        </Popover>
+      )}
+    </Panel>
+  );
+};
 
 const mapState = state => ({
   noBackground: state.app.noBackground,
