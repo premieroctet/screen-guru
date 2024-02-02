5c204ed29e0d05f04fd0a12a69d8c185036684cb
Thibault <thibault@premieroctet.com>
2024-02-02 18:35:10 +0100

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import { init } from '@rematch/core';
 import { Provider } from 'react-redux';
+import { Analytics } from '@vercel/analytics/react';
+
 import models from '../models';
 import 'react-github-corners/dist/GithubCorner.css';
 import '../styles/globals.css';
@@ -11,8 +13,11 @@ const store = init({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+      <Analytics />
+    </>
   );
 }
